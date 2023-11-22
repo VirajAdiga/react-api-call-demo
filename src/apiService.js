46f84318b523686b3d7c3273cc32610feb54bfbb
VirajAdiga <vadigathirthahall@deloitte.com>
2023-11-22 12:00:52 +0530

    Create api service to call external images api

---
+++ src/apiService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.unsplash.com/"
+const URL_PATH = "search/photos"
+const API_KEY = ""
+
+async function searchImages(searchTerm){
+    const response = await axios.get(BASE_URL + URL_PATH, {
+        headers: {
+            Authorization: "Client-ID " + API_KEY
+        },
+        params: {
+            query: searchTerm
+        }
+    });
+    return response.data.results;
+}
+
+export default searchImages;
